7e202ae3731bdf9b78b3c897252cfd5b68c75ac1
Takatomo Ezo <takatomo@e20.jp>
2022-07-31 08:27:21 +0900

    Install git-flow and tmux

---
+++ installer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh
+sudo apt install -y tmux
