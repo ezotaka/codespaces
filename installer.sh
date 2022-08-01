051128f02a126756225e826be1709d4c2e49ee54
ezotaka <34199555+ezotaka@users.noreply.github.com>
2022-08-01 22:43:33 +0900

    Remove gitflow directory

--- installer.sh
+++ installer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh
+wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh; rm -r gitflow
 sudo apt install -y tmux
